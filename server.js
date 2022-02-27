ee99421449e6e32611f6c4d847ec77315d66956e
Rajat Jain <rajat.apple007@gmail.com>
2022-02-27 13:15:31 -0800

    NewServerSelection Error

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const ShortUrl = require('./Models/shortUrl')
 
-mongoose.connect('mongodb://localhost/urlShortener',{
+mongoose.connect('mongodb://127.0.0.1/UrlShortener',{
 useNewURLParser: true, useUnifiedTopology:true
 })
+.then(() => console.log('MongoDB Connected...'))
+.catch((err) => console.log(err))
+
 app.set('view engine','ejs')
+app.use(express.urlencoded({extended:false}))
 
-app.listen(process.env.PORT||4000);
 
-app.get('/',(req,res)=>{
-    res.render('index')
+
+app.get('/',async(req,res)=>{
+    const shortUrls = await ShortUrl.find();
+    res.render('index',{shortURLs:shortUrls})
 })
 
-app.post('/shortURLs',(req,res)=>{
-
+app.post('/shortUrls',async(req,res)=>{
+    await ShortUrl.create({full:req.body.fullURL})
+    res.redirect('/')
 })
+
+app.get('/:shortUrl',async(req,res)=>{
+    const shortUrl = await ShortUrl.findOne({short:req.params.shortUrl})
+    if(shortUrl==null) return res.sendStatus(404)
+    shortUrl.clicks++
+    shortUrl.save()
+    res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT||27017);
